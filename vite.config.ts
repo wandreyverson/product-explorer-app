f22a3f5fc5b244743f721f363d9fc2a072930883
Wandreyverson <wandreyverson.js@gmail.com>
2026-02-18 03:39:02 -0300

    fix deploy vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // ✅ ADICIONE ESTA LINHA
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
